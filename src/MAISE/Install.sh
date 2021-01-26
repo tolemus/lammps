59bffda3b2fd43d9f032d860e9b395e14da9ec14
Ethan Ferguson <sometimescode@gmail.com>
2021-01-25 20:37:19 -0500

    uninstalling works

--- src/MAISE/Install.sh
+++ src/MAISE/Install.sh
@@ -8,6 +8,11 @@ if (test $1 = 1) then
     sed -i -e "s|^PKG_INC.=[ \t]*|&$maise_INC |" ../Makefile.package
     sed -i -e "s|^PKG_LIB.=[ \t]*|&$maise_LIB |" ../Makefile.package
   fi
+elif (test $1 = 0) then
+  if (test -e ../Makefile.package) then
+    sed -i -e "s|\(^PKG_INC.=.*\)\($maise_INC\)\(.*\)|\1\3|" ../Makefile.package
+    sed -i -e "s|\(^PKG_LIB.=.*\)\($maise_LIB\)\(.*\)|\1\3|" ../Makefile.package
+  fi
 else
-    echo Unknown \$1 $1
+  echo Unknown \$1 $1
 fi
