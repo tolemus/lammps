8f4865d4bcd5a98f92861550bc78bd108fd0ca5e
Ethan Ferguson <sometimescode@gmail.com>
2021-01-26 13:34:04 -0500

    Installing is nicer

--- src/MAISE/Install.sh
+++ src/MAISE/Install.sh
@@ -1,18 +1,21 @@
 
-maise_INC="-I../MAISE/inc -I../MAISE/lib/include"
-maise_LIB="-lmaise -lgomp -L../MAISE/lib"
+maise_MAKE="include ../MAISE/Makefile.maise"
 
 if (test $1 = 1) then
-
+  # Install
+  sed -i -e "\|$maise_MAKE|d" ../Makefile.package.settings
+  sed -i -e 's|\(^PKG_INC.=.*\)\($(maise_INC)\)\(.*\)|\1\3|' ../Makefile.package
+  sed -i -e 's|\(^PKG_LIB.=.*\)\($(maise_LIB)\)\(.*\)|\1\3|' ../Makefile.package
   if (test -e ../Makefile.package) then
-    sed -i -e "s|^PKG_INC.=[ \t]*|&$maise_INC |" ../Makefile.package
-    sed -i -e "s|^PKG_LIB.=[ \t]*|&$maise_LIB |" ../Makefile.package
+    sed -i -e "\|$maise_MAKE|d" ../Makefile.package.settings
+    echo $maise_MAKE >> ../Makefile.package.settings
+    sed -i -e 's|^PKG_INC.=[ \t]*|&$(maise_INC) |' ../Makefile.package
+    sed -i -e 's|^PKG_LIB.=[ \t]*|&$(maise_LIB) |' ../Makefile.package
   fi
-elif (test $1 = 0) then
-  if (test -e ../Makefile.package) then
-    sed -i -e "s|\(^PKG_INC.=.*\)\($maise_INC\)\(.*\)|\1\3|" ../Makefile.package
-    sed -i -e "s|\(^PKG_LIB.=.*\)\($maise_LIB\)\(.*\)|\1\3|" ../Makefile.package
-  fi
+# elif (test $1 = 0) then
+  # Uninstall
+  # if (test -e ../Makefile.package) then
+  # fi
 else
   echo Unknown \$1 $1
 fi
