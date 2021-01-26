6bbf8ca8e86e629d32658c07cef4c8410703343f
Ethan Ferguson <sometimescode@gmail.com>
2021-01-26 13:37:41 -0500

    Uninstalling should also work flawlessly

--- src/MAISE/Install.sh
+++ src/MAISE/Install.sh
@@ -6,14 +6,16 @@ if (test $1 = 1) then
   sed -i -e "\|$maise_MAKE|d" ../Makefile.package.settings
   sed -i -e 's|\(^PKG_INC.=.*\)\($(maise_INC)\)\(.*\)|\1\3|' ../Makefile.package
   sed -i -e 's|\(^PKG_LIB.=.*\)\($(maise_LIB)\)\(.*\)|\1\3|' ../Makefile.package
+  cp ../MAISE/*_maise.* ..
   if (test -e ../Makefile.package) then
     sed -i -e "\|$maise_MAKE|d" ../Makefile.package.settings
     echo $maise_MAKE >> ../Makefile.package.settings
     sed -i -e 's|^PKG_INC.=[ \t]*|&$(maise_INC) |' ../Makefile.package
     sed -i -e 's|^PKG_LIB.=[ \t]*|&$(maise_LIB) |' ../Makefile.package
   fi
-# elif (test $1 = 0) then
+elif (test $1 = 0) then
   # Uninstall
+  rm ../*_maise.*
   # if (test -e ../Makefile.package) then
   # fi
 else
