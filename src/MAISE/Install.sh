132ba2de0323d2b0f35ca06670516fa0f475cf52
Ethan Ferguson <sometimescode@gmail.com>
2021-01-25 20:29:16 -0500

    Install should work now, just do 'make yes-MAISE

--- src/MAISE/Install.sh
+++ src/MAISE/Install.sh
@@ -1,1 +1,13 @@
-echo AAAAAAAA
+
+maise_INC="-I../MAISE/inc -I../MAISE/lib/include"
+maise_LIB="-lmaise -lgomp -L../MAISE/lib"
+
+if (test $1 = 1) then
+
+  if (test -e ../Makefile.package) then
+    sed -i -e "s|^PKG_INC.=[ \t]*|&$maise_INC |" ../Makefile.package
+    sed -i -e "s|^PKG_LIB.=[ \t]*|&$maise_LIB |" ../Makefile.package
+  fi
+else
+    echo Unknown \$1 $1
+fi
